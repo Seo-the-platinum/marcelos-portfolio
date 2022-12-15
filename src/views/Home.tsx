13ead20c39b00f9f8285cfde83d4031114032dab
Eliseo Flores <eliseo_flores530@yahoo.com>
2022-12-15 14:30:45 -0800

    added react-anchor-link packageto handle routing within same page. Added test code

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,13 +3,21 @@ import Skills from '../components/Skills'
 import Profile from '../components/profile/Profile'
 import Work from './Work'
 import Demo from '../components/demos/Demos'
+import AnchorLink from 'react-anchor-link-smooth-scroll-v2'
 import './css/views.css'
 
 const Home = () => {
   return (
     <div className='homeContainer'>
+      <AnchorLink href='#innerTest'> goToTest</AnchorLink>
       <Profile/>
       <Demo/>
+      <div id="test" style={{height: '500px', display: 'flex', flexDirection: 'column', justifyContent: 'flex-end'}}>
+        heres the test stuff
+        <div id='innerTest'>
+          heres where we should be
+        </div>
+      </div>
     </div>
   )
 }
