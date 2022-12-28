4378ebd0a360989f19c420ee9179ef6da5ac9098
Eliseo Flores <eliseo_flores530@yahoo.com>
2022-12-27 21:44:23 -0800

    changed color for headings and hamburger button

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -4,7 +4,6 @@ import './Profile.css'
 const Profile = () => {
   return (
     <div className='profileContainer'>
-      {/* <img className='profilePic' src='images/Headshot.jpeg'/> */}
       <h3>Marcelo Flores</h3>
       <p>Voiceover</p>
     </div>
