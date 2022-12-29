dc0be110345c414f2eb288a2f8defe7f2534c4ac
Eliseo Flores <eliseo_flores530@yahoo.com>
2022-12-28 21:31:15 -0800

    made changes to profile image positioning and demo sizes

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -6,8 +6,12 @@ import './css/views.css'
 const Home = () => {
   const windowWidth = window.innerWidth
   return (
-    <div className='homeContainer' id='home' style={{backgroundImage: windowWidth > 479 ? 'url(/images/Headshot_4.jpg)': 'none'}}>
+    <div 
+      className='homeContainer' 
+      id='home' 
+      style={{backgroundImage: windowWidth > 479 && windowWidth < 769 ? 'url(/images/Headshot_4.jpg)': 'none'}}>
       <Profile/>
+      {windowWidth > 769 && <img className='headshot' src='/images/Headshot_4.jpg'/>}
       <Demo/>
     </div>
   )
