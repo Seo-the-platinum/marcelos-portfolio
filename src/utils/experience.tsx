90ea0796883bdce54ad7aed0685e997b49e14c13
Eliseo Flores <eliseo_flores530@yahoo.com>
2023-09-26 18:23:09 -0700

    added additional work history

--- src/utils/experience.tsx
+++ src/utils/experience.tsx
@@ -1,39 +1,45 @@
 export const experience = [
-    {
-        link: "https://www.youtube-nocookie.com/embed/Vxg5t7CwVHE",
-        role: 'Shane',
-        title: 'Last Day',
-        type: 'Video'
-    },
-    {
-        link: 'images/metaverse101.jpg',
-        role: 'Narrator',
-        title: 'Metaverse 101',
-        type: 'image'
-    },
-    {
-        link: "https://www.youtube-nocookie.com/embed/xJGloY3gsto",
-        role: 'Liam',
-        title: 'Dear Diary...Would he like me if I was prettier?',
-        type: 'Video'
-    },
-    {
-        link: 'https://www.youtube-nocookie.com/embed/ELTk_Ew2Pds',
-        role: 'Raihan',
-        title: `Iono's Steam Clip`,
-        type: 'Video'
-    },
-    {
-        link: 'https://www.youtube-nocookie.com/embed/PnLFUnA-mO8',
-        role: 'Miku',
-        title: 'Trans Sawk',
-        type: 'Video'
-    },
-    {
-        link: 'https://www.youtube-nocookie.com/embed/EW97f0fZmWA',
-        role: 'Miku',
-        title: 'Trans Sawk 1.2',
-        type: 'Video'
-    }
+  {
+    link: 'https://www.youtube-nocookie.com/embed/0FvPPt1QMe8',
+    role: 'Narrator',
+    title: 'Tempo Microwavable Protein Packed Meals',
+    type: 'Video'
+  },
+  {
+    link: "https://www.youtube-nocookie.com/embed/Vxg5t7CwVHE",
+    role: 'Shane',
+    title: 'Last Day',
+    type: 'Video'
+  },
+  {
+    link: 'images/metaverse101.jpg',
+    role: 'Narrator',
+    title: 'Metaverse 101',
+    type: 'image'
+  },
+  {
+    link: "https://www.youtube-nocookie.com/embed/xJGloY3gsto",
+    role: 'Liam',
+    title: 'Dear Diary...Would he like me if I was prettier?',
+    type: 'Video'
+  },
+  {
+    link: 'https://www.youtube-nocookie.com/embed/ELTk_Ew2Pds',
+    role: 'Raihan',
+    title: `Iono's Steam Clip`,
+    type: 'Video'
+  },
+  {
+    link: 'https://www.youtube-nocookie.com/embed/PnLFUnA-mO8',
+    role: 'Miku',
+    title: 'Trans Sawk',
+    type: 'Video'
+  },
+  {
+    link: 'https://www.youtube-nocookie.com/embed/EW97f0fZmWA',
+    role: 'Miku',
+    title: 'Trans Sawk 1.2',
+    type: 'Video'
+  },
 ]
 
